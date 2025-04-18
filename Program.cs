c98d5889b3a6037ad29661a8161af0c3df8080aa
Taylo <Taylo@MARIEL>
2025-04-17 23:01:19 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Vehicle
+{ 
+    GroundVehicle; Vehicle
+
+
+    car;GroundVehicle
+    Truck; GroundVehicle 
+    Motorcycle;GroundVehicle
+
+
+    AirVehicle; Vehicle
+    Airplane; AirVehicle
